db8ef83a83bdb480463fd5633126faa9dff9ae86
Astrian Zheng <happy.wheel6743@astrian.moe>
2024-04-08 19:26:22 +1000

    global connection pool

--- backend/utils/mariadb.ts
+++ backend/utils/mariadb.ts
@@ -1,12 +1,17 @@
 import mariadb from 'mariadb'
 
+let pool: mariadb.Pool | null = null
+
 export default async () => {
-  const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port: parseInt(process.env.DB_PORT || '3306'),
-  })
+  if (!pool) {
+    pool = mariadb.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      connectionLimit: 10,
+    })
+  }
   return pool
 }
